df891a7da6bad47161205ef7c1ad51b24e540d01
Krypel20 <oleole999@interia.pl>
2024-03-25 10:27:19 +0100

    work in progress

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@ from discord import Interaction
 from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 #custom imports
 TOKEN = os.getenv('TOKEN')
-import responses
+from async_commands import send_message
 
 
 polish_to_english_month = {
@@ -47,14 +47,60 @@ flags_emojis = {
 }
 
 
-async def send_message(message, user_message, is_private):
-    try:
+def convert_to_Warsaw_time(date_times):
+    new_times = []
+    for czas in date_times:
+        h, m = map(int, czas.split(':'))
+        h += 1
+        if h == 24:  # Jeśli godzina wynosi 24, to zmieniamy na 00
+            h = 0
+        new_time = f"{h:02d}:{m:02d}"
+        new_times.append(new_time)
+    return new_times
+
+def convert_date_to_weekday(date_strings):
+    converted_dates = []
+    
+    for date_string in date_strings:
+        for polish_month, english_month in polish_to_english_month.items():
+            if polish_month in date_string:
+                date_string = date_string.replace(polish_month, english_month)
+                break
         
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-        
-    except Exception as e:
-        print(e)
+        date_object = datetime.strptime(date_string, '%d %b')
+        weekday_num = (date_object.weekday() + 1) % 7
+        weekdays = ['Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb', 'Nd']
+        weekday_name = weekdays[weekday_num]
+        converted_dates.append(weekday_name)
+    
+    return converted_dates
+
+def convert_date_to_polish_months(date_strings):
+    polish_dates = []
+    for date_string in date_strings:
+        for polish_month, english_month in polish_to_english_month.items():
+            if english_month in date_string:
+                day = date_string.split()[0]
+                polish_dates.append(f"{day} {polish_month}")
+                break
+    return polish_dates
+            
+def check_session_status(current_datetime, session_datetime):
+
+    session_duration = timedelta(hours=1, minutes=30)
+    
+    if session_datetime < current_datetime:
+        if current_datetime - session_datetime >= session_duration:
+            return "⚫"
+        else:
+            return "🟢"
+    elif session_datetime > current_datetime:
+        return "🔴"
+    else:
+        return "🟢"
+
+def next_session_starts_in():
+    pass
 
 def run_discord_bot():
     intents = discord.Intents.default() # or discord.Intents.all()
@@ -77,10 +123,10 @@ def run_discord_bot():
     COOLDOWN_SECONDS = 10
     @client.hybrid_command(name="f1", description="Info about current race week")
     @commands.cooldown(1, COOLDOWN_SECONDS, commands.BucketType.default)
+    
     async def f1(ctx):
         calendar_url = "https://f1calendar.com/pl"
         official_schedule = "https://www.formula1.com/en/racing/2024.html"
-        
         response = requests.get(calendar_url)
         soup = BeautifulSoup(response.content, 'html.parser')
         races = soup.find_all("tbody")
@@ -91,9 +137,7 @@ def run_discord_bot():
             get_flag_emoji = lambda race_id: flags_emojis.get(race_id, ":question:")
             
             if race_status:
-                
                 race_name = race.find("span", class_='').text
-                
                 dates = race.find_all('td', class_="text-right md:text-left")
                 date_times = race.find_all('div', class_="text-right md:text-left pr-2 md:pr-0", string=True)
                 
@@ -101,22 +145,53 @@ def run_discord_bot():
                 sessions_name = ["FP1", "FP2", "FP3", "Kwalifikacje", "Wyścig"]
 
                 dates = [date.text for date in dates]
-                dates = convert_date_to_weekday(dates)
+                
+                datesPL = convert_date_to_polish_months(dates)
+                week_start = datesPL[0]
+                week_end = datesPL[-1]
+                
+                weekdays = convert_date_to_weekday(dates)
                 date_times = [time.text for time in date_times]
                 date_times = convert_to_Warsaw_time(date_times)
                 
-                race_week_datetimes = list(zip(sessions_name, dates, date_times))
-                embed = discord.Embed(title=f"{get_flag_emoji(race_id)} {race_name} {get_flag_emoji(race_id)}", color=0xff0000)
-                racetrack_img = "https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/Track%20icons%204x3/Australia.png.transform/2col/image.png"
+                race_week_datetimes = list(zip(sessions_name, dates, weekdays, date_times))
+                embed = discord.Embed(title=f"{get_flag_emoji(race_id)} {race_name} {get_flag_emoji(race_id)} ‏ ‏ ‎ ‎ ‎ {week_start} - {week_end} ", color=0xEF1A2D)
+                thumbnail = "https://cdn.discordapp.com/emojis/734895858725683314.webp?size=96&quality=lossless"
                 embed.add_field(name="", value="", inline=False)
-                embed.set_thumbnail(url=racetrack_img)
+                embed.set_thumbnail(url=thumbnail)
                 
-                for session, date, time in race_week_datetimes:
+                current_datetime = datetime.now()
+                count_timer = ''
+                upcoming_session = False
+                
+                for session, date, weekday, time in race_week_datetimes:
+                    session_datetime = datetime.strptime(f"{date} {time} {current_datetime.year}", "%d %b %H:%M %Y")
                     print(f"{session}: {date} {time}")
-                    embed.add_field(name="", value=f":red_circle: **{session}**", inline=True)
-                    embed.add_field(name="", value=f":calendar_spiral: {date}", inline=True)
-                    embed.add_field(name="", value=f":alarm_clock: {time}", inline=True)
-                    embed.add_field(name="", value=f"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━", inline=False)
+                    embed.add_field(name="", value=f"{check_session_status(current_datetime, session_datetime)} **{session}**", inline=True)
+                    embed.add_field(name="", value=f":calendar_spiral: {weekday}", inline=True)
+                    embed.add_field(name="", value=f":alarm_clock: **{time}**", inline=True)
+                    
+                    if not upcoming_session:
+                        if check_session_status(current_datetime, session_datetime) == "🔴":
+                            time_left = session_datetime - current_datetime
+                            hours = time_left.seconds // 3600
+                            minutes = (time_left.seconds % 3600) // 60
+                            time_left = f"{hours} h {minutes} m"
+                            
+                            embed.add_field(name="", value=f':clock1: **Rozpocznie sie za {time_left}**', inline=False)
+                            upcoming_session = True
+
+                        elif check_session_status(current_datetime, session_datetime) == "🟢":
+                            embed.add_field(name="", value=f'**🟢TRWA🟢**', inline=False)
+                            upcoming_session = True
+                        else: 
+                            count_timer = "⚫KONIEC⚫"
+                    
+                    
+                            
+                    embed.add_field(name="", value=f"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━", inline=False)
+                    
+                print(count_timer)
 
                 await ctx.send(embed=embed)
                 await client.tree.sync()
@@ -148,31 +223,3 @@ def run_discord_bot():
             await send_message(message, user_message, is_private=False)
      
     client.run(TOKEN)
-
-def convert_to_Warsaw_time(date_times):
-    new_times = []
-    for czas in date_times:
-        h, m = map(int, czas.split(':'))
-        h += 1
-        if h == 24:  # Jeśli godzina wynosi 24, to zmieniamy na 00
-            h = 0
-        new_time = f"{h:02d}:{m:02d}"
-        new_times.append(new_time)
-    return new_times
-
-def convert_date_to_weekday(date_strings):
-    converted_dates = []
-    
-    for date_string in date_strings:
-        for polish_month, english_month in polish_to_english_month.items():
-            if polish_month in date_string:
-                date_string = date_string.replace(polish_month, english_month)
-                break
-        
-        date_object = datetime.strptime(date_string, '%d %b')
-        weekday_num = (date_object.weekday() + 1) % 7
-        weekdays = ['Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb', 'Nd']
-        weekday_name = weekdays[weekday_num]
-        converted_dates.append(weekday_name)
-    
-    return converted_dates
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-await ctx.send("Nie udało się pobrać informacji o najbliższym wyścigu.")   
+datesPL = convert_date_to_polish_months(dates)
+                # week_start = datesPL[0]
+                # week_end = datesPL[-1]
